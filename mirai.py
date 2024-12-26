b057330c4145644567921f6737326d9a449523c5
minematsu toshiya <mine.webstaff@gmail.com>
2024-12-26 17:41:10 +0900

    mirai-compass 操作用
    loginまではうまく動いている。
    
    get_by_role と locator のチェインで、大体うまくいきそう。

---
+++ mirai.py
@@ -0,0 +1,24 @@
+from playwright.sync_api import sync_playwright
+
+
+class MiraiAdmin():
+    def __init__(self):
+        self.playwright = sync_playwright().start()
+        self.browser = self.playwright.chromium.launch(headless=False)
+        self.page = self.browser.new_page()
+
+    def close(self):
+        self.browser.close()
+        self.playwright.stop()
+
+    def login(self, id:str, passwd:str):
+        self.page.goto("https://mirai-compass.net/adm/schAdmin/login.jsf")
+        self.page.get_by_role('textbox').locator('nth=0').fill(id)
+        self.page.get_by_role('textbox').locator('nth=1').fill(passwd)
+        self.page.get_by_role('button').click()
+
+    def goto_application():
+        pass
+
+    def goto(self, url):
+        self.page.goto(url)
